b084e9c5d8439405e3ee733ff03b7ccb609b3635
Smerlyn A. García Valenciano <78616922+Nylrems@users.noreply.github.com>
2022-03-30 11:25:17 -0400

    Botón de pedir cartas configurado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,8 +6,15 @@
 let deck = [];
 const tipos = ["H", "S", "C", "D"];
 const especiales = ["A", "J", "K", "Q"];
+let puntosJugador = 0,
+  puntosComputadora = 0;
 
-//Esta función crear una nueva baraja
+//?Referencias del HTML
+const btnPedir = document.querySelector("#btnPedir");
+const divCartasJugador = document.querySelector("#jugador-cartas");
+const smallsPuntos = document.querySelectorAll("small");
+
+//*Esta función crear una nueva baraja
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) {
@@ -20,11 +27,73 @@ const crearDeck = () => {
     }
   }
   deck = _.shuffle(deck);
-  console.log(deck);
+  // console.log(deck);
 
   return deck;
 };
 
 crearDeck();
 
+//*Esta función me permite tomar una carta
+const pedirCarta = () => {
+  // let rand = Math.floor(Math.random()*deck.length);
+  // carta = deck[rand]
+  // deck.splice(rand, 1)
 
+  if (deck.length === 0) {
+    throw "No hay cartas en el Deck";
+  }
+
+  const carta = deck.pop();
+  // console.log(deck);
+  // console.log(carta);
+  return carta;
+};
+
+// for(let i = 0; i <100; i++)
+// pedirCarta();
+
+//*Esta función me permite pedir una carta
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+
+  // if (isNaN(valor)) {
+  //   puntos = (valor === 'A') ? 11: 10;
+  // } else {
+  //   console.log("Es un número");
+  //   puntos = valor * 1;
+  // }
+
+  //return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+
+  let puntos;
+  puntos = isNaN(valor)
+    ? (puntos = valor === "A" ? 11 : 10)
+    : (puntos = valor * 1);
+  return puntos;
+};
+
+// let valor = valorCarta(pedirCarta());
+// console.log({ valor });
+
+//?Eventos
+btnPedir.addEventListener("click", () => {
+  const carta = pedirCarta();
+  console.log(carta);
+
+  puntosJugador = puntosJugador + valorCarta(carta);
+  smallsPuntos[0].innerText = puntosJugador;
+
+  const imgCarta = document.createElement("img");
+  imgCarta.src = `assets/cartas/${carta}.png`;
+  imgCarta.classList.add("carta");
+  divCartasJugador.append(imgCarta);
+
+  if (puntosJugador > 21) {
+    console.warn("Perdiste");
+    btnPedir.disabled = true;
+  } else if (puntosJugador === 21) {
+    console.warn("21 blackjack!!!");
+    btnPedir.disabled = true;
+  }
+});
